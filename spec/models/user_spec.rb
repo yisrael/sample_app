090f725f059894ba8c37e7d747f2717386b15ab3
Yisrael Aminov <Yisrael.A@fiverr.com>
2013-04-14 23:17:35 +0300

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,7 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
@@ -123,5 +124,10 @@ describe User do
   describe "with upcase e-mail" do
     before { @user.email.upcase! }
     it { should be_valid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
